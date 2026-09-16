b1690205a759ba06109c7a0ed306d59e368b97d6
Devansh Khandelwal <2023pceaddevansh018@poornima.org>
2026-09-16 11:06:19 +0000

    Fix server path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(__dirname));
 
 let equipment = [
   { id: 1, name: 'Camera', available: 3 },
